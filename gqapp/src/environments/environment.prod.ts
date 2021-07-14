be04fae3ccfc003d8a46d881f7e5dca2073ea542
Erik Wegner <E_Wegner@web.de>
2021-07-14 20:19:05 +0200

    Routing

--- gqapp/src/environments/environment.prod.ts
+++ gqapp/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apptitle: '',
-  welcometext: '',
+  apptitle: (window as any).gebquiz_apptitle || 'Quiz game',
+  welcometext: (window as any).gebquiz_welcometext || 'Answer some questions and earn points. Try to score as much as possible and claim your place in the high score.',
   production: true
 };
